978bbbab30ef72d65682bb54649049e282b8fe91
marc.mostazo <marc.mostazo@estudiantat.upc.edu>
2023-10-26 09:33:43 +0200

    Alfabet.java creadores i gets

--- FONTS/Domini/Alfabet.java
+++ FONTS/Domini/Alfabet.java
@@ -1,9 +1,34 @@
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Vector;
+import java.util.HashSet;
+import java.util.Set;
+//import java.util.Vector;
 package Domini
 public class Alfabet {
     private String idioma;
-    private String lletres;
+    private Set<char> lletres;
+
+    //Pre: lletres conté totes les lletres de l'alfabet donat
+    //Post: Es crea un alfabet d'idioma = idioma y lletres=lletres
+    public Alfabet (String idioma, Set<char> lletres) {
+        this.idioma = idioma;
+        this.lletres = lletres;
+    }
+
+    //pre:
+    //Post: Es crea un alfabet idioma = idioma
+    public Alfabet (String idioma) {
+        this.idioma = idioma;
+    }
+
+    //Retorna idioma de l'alfabet
+    public String getIdioma() {
+        return idioma;
+    }
+
+    //Retorna les lletres de l'alfabet
+    public set<char> getLletres() {
+        return lletres
+    }
+
+
 }
