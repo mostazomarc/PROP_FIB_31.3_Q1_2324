c081d5b94122bb5ac5ac9ae18caaed8029f0d951
marc.mostazo <marc.mostazo@estudiantat.upc.edu>
2023-10-30 17:00:01 +0100

    creadores llistes

--- FONTS/Domini/LlistaFrecuencies.java
+++ FONTS/Domini/LlistaFrecuencies.java
@@ -6,4 +6,12 @@ import java.util.Map;
 public class LlistaFrecuencies {
     private String nom;
     private Map<String, int> LListaParaules;
+
+    public LlistaFrecuencies (String nom) {
+        //input arxius
+    }
+
+    public LlistaFrecuencies (String nom, Map<pair<char,char>, int> LListaParaules) {
+
+    }
 }
