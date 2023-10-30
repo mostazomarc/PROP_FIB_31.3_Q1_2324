44c6f5aad0d6d43e5a7b0a88dc1d259c5ff3d49e
marc.mostazo <marc.mostazo@estudiantat.upc.edu>
2023-10-30 15:43:15 +0100

    Atributs inicials, funcions d'inicialització i getsPerfil

---
+++ FONTS/ControladorsDomini/CtrlDomini.java
@@ -0,0 +1,46 @@
+package ControladorsDomini;
+
+import Domini.*;
+
+public class CtrlDomini {
+    private Perfil PerfilActual; //Perfil que esta usant actualment el programa
+    private String Estrategia; //Estrategia utilitzada en la fabricació del teclat
+    private HashMap <String, Perfil> PerfilsActius; //Conjunt d'usuaris registrats
+    private static CtrlDomini singletonObject;
+
+
+    //Pre:
+    //Post: Es crea una instancia de domini.
+    public CtrlDomini(){
+        inicialitzar();
+    }
+
+    //Pre:
+    //Post: Retorna la instancia de Controlador Domini. Si no existeix cap instancia de CtrlDomini, es crea.
+    public static CtrlDomini getInstance(){ //CtrlDomini es singleton
+        if(singletonObject == null){
+            singletonObject = new CtrlDomini();
+        }
+        return singletonObject;
+    }
+
+    //Pre:
+    //Post: S'inicialitzen les variables necessaries.
+    public void inicialitzar() {
+        Estrategia = "QAP"; //estrategia per defecte
+        PerfilsActius = new HashMap<String, Perfil>()
+    }
+
+    //Pre:
+    //Post: Retorna el conjunt de perfils.
+    public HashMap<String, Perfil> getAllPerfils() {
+        return PerfilsActius;
+    }
+
+    //Pre:
+    //Post: Retorna la instancia del Perfil.
+    public Perfil getPerfil(String id) {
+        if (PerfilsActius.containsKey(id)) return PerfilsActius.get(id);
+        else return null //perfil no existeix
+    }
+}
