3cb3d19b0ee6116b48f328db89af273bcdff3edb
marc.mostazo <marc.mostazo@estudiantat.upc.edu>
2023-11-07 16:05:36 +0100

    Tests Alfabet/Idioma creats

---
+++ FONTS/JUnit/AlfabetTest.java
@@ -0,0 +1,14 @@
+package JUnit;
+
+import Domini.*;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+
+public class AlfabetTest {
+
+}
