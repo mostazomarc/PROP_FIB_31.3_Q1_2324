42bef48c61e5fa9ecc8c613c26c3da260698472e
marc.mostazo <marc.mostazo@estudiantat.upc.edu>
2023-11-07 17:02:20 +0100

    Stubs Idioma i CreadoraTeclat

---
+++ FONTS/Stubs/IdiomaStub.java
@@ -0,0 +1,13 @@
+package Stubs;
+
+
+import java.util.*;
+
+public class IdiomaStub {
+
+    private char[] lletres = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+    public char[] getLletres() {
+        return lletres;
+    }
+}
