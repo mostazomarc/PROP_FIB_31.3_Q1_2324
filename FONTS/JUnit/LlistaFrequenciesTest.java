1406856e786a7553dcdbe8f1e8bd101d7c5a0676
marc.mostazo <marc.mostazo@estudiantat.upc.edu>
2023-11-07 15:12:24 +0100

    test llistaFrequencies

--- FONTS/JUnit/LlistaFrequenciesTest.java
+++ FONTS/JUnit/LlistaFrequenciesTest.java
@@ -3,14 +3,49 @@ package JUnit;
 import Domini.*;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LlistaFrequenciesTest {
-    @Test
-    public void creadoraLlistaPerfil() {
-        LlistaFrequencies llistaEsperada = new LlistaFrequencies("NovaLlista");
-        Perfil perfilProva = new Perfil("Prova");
-        LlistaFrequencies resultat = perfilProva.crearLlistaFreq("NovaLlista");
-        assertEquals(llistaEsperada.getNom(),resultat.getNom());
+    private Map<String, Integer> llistaParaulesProva = new HashMap<>();
+
+    @Before
+    public void omplirLlistaProva() {
+        llistaParaulesProva.put("Hola", 10);
+        llistaParaulesProva.put("Casa", 20);
+        llistaParaulesProva.put("Adeu", 30);
+        llistaParaulesProva.put("Menjar", 30);
+        llistaParaulesProva.put("Fideu", 30);
     }
+
+    @Test
+    //Creadora LLista amb nom i getNom
+    public void creadoraLlistaNom() {
+        LlistaFrequencies resultat = new LlistaFrequencies("NovaLlista");
+        assertEquals("NovaLlista",resultat.getNom());
+    }
+
+    @Test
+    //Creadora LLista amb nom i Llista de paraules i getFrequencies
+    public void creadoraLlistaNomLlista() {
+
+        LlistaFrequencies resultat = new LlistaFrequencies("NovaLlista", llistaParaulesProva);
+        assertEquals(llistaParaulesProva,resultat.getFrequencies());
+    }
+
+    @Test
+    //Insertar llista de frequencies a una llista ja creada
+    public void insertarFrequencies() {
+
+        LlistaFrequencies llistaProva = new LlistaFrequencies("NovaLlista");
+        llistaProva.insertarFrequencies(llistaParaulesProva);
+        assertEquals(llistaParaulesProva,llistaProva.getFrequencies());
+    }
+
+
 }
