3581e5890ecd28593ef4969be44f018feb40b9a6
Arnau Tajahuerce <arnau.tajahuerce@estudiantat.upc.edu>
2023-11-09 08:56:25 +0100

    Test Alfabet

--- FONTS/JUnit/AlfabetTest.java
+++ FONTS/JUnit/AlfabetTest.java
@@ -10,5 +10,26 @@ import org.junit.Test;
 import java.util.*;
 
 public class AlfabetTest {
+    private String nomAlfabet;
+    private Set<Character> lletresProva = new HashSet<Character>();
+
+    @Before
+    public void omplirLletres() {
+        for (char lletra = 'a'; lletra <= 'z'; lletra++) lletresProva.add(lletra);
+    }
+
+    @Test
+    public void creadoraAlfebetNomLletres() {
+        Alfabet resultat = new Alfabet("Llatí", lletresProva);
+        assertEquals("Llatí",resultat.getNomAlfabet());
+        assertEquals(lletresProva,resultat.getLletres());
+    }
+
+    @Test
+    public void getNumLletres() {
+        int numLletres = 26;
+        Alfabet resultat = new Alfabet("Llatí", lletresProva);
+        assertEquals(numLletres,resultat.getNumLletres());
+    }
 
 }
