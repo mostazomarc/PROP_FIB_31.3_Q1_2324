d6f0084d9b94af9355eee6cefcfc161318403e9c
Arnau Tajahuerce <arnau.tajahuerce@estudiantat.upc.edu>
2023-11-18 15:27:56 +0100

    Comentaris de les funcions

--- FONTS/Domini/Idioma.java
+++ FONTS/Domini/Idioma.java
@@ -18,7 +18,7 @@ public class Idioma {
     }
 
     //Pre:
-    //Post: Es crea un Idioma amb nom, alfabet i llista de frequencies predeterimnada
+    //Post: Es crea un Idioma amb nom, alfabet i llista de freqüències predeterimnada
     public Idioma (String nom, Alfabet alfabet, LlistaFrequencies llistaPred) {
         this.nom = nom;
         this.alfabet = alfabet;
@@ -27,21 +27,22 @@ public class Idioma {
     }
 
     //Pre:
-    //Post: Es crea un Idioma amb nom, alfabet i una nova llista de frequencies predeterimnada
-    public Idioma (String nom, Alfabet alfabet, String nomLlista, Map<String, Integer> llistaParaules) {
+    //Post: Es crea un Idioma amb nom, alfabet i una NOVA llista de freqüències predeterimnada
+    public Idioma (String nom, Alfabet alfabet, Map<String, Integer> llistaParaules) {
         this.nom = nom;
         this.alfabet = alfabet;
         alfabet.afegirIdioma(nom);
         new LlistaFrequencies("LlistaPred"+nom, this, llistaParaules); //Aquesta creadora ja vincula la llista de freqüències a aquest idioma
     }
 
-    //Pre: No existeix un idioma amb nomIdioma. Existeix l'alfabet alphabet. Existeix la llista de freqüències FreqList
-    //Post: S'ha creat un idioma amb nom = nomIdioma, alfabet = alphabet i llistaFreq = FreqList
-    //      i s'afegeix aquest idioma a l'alfabet donat
+    //Pre: La llistaFreq existeix
+    //Post: S'afegeix la llistaFreq com a llista de freqüències predeterminada de l'idioma
     public void afegirLlistaFreqPredeterminada (LlistaFrequencies llistaFreq) {
         if (llistaFreqPredeterminada == null) llistaFreqPredeterminada = llistaFreq;
     }
 
+    //Pre: La llistaFreq existeix
+    //Post: llistaFreq és ara la nova llista de freqüències predeterminada de l'idioma
     public void canviarLlistaFreqPredeterminada (LlistaFrequencies llistaFreq) {
         llistaFreqPredeterminada = llistaFreq;
     }
@@ -51,6 +52,7 @@ public class Idioma {
         return nom;
     }
 
+    //Retorna l'alfabet de l'idioma
     public Alfabet getAlfabet() { return alfabet; }
 
     //Retorna les lletres que té l'alfabet de l'idioma
@@ -58,17 +60,17 @@ public class Idioma {
         return alfabet.getLletres();
     }
 
-    //Retorna la llista de frequències de l'idioma
+    //Retorna la llista de frequencies de l'idioma
     public LlistaFrequencies getLlistaFreq() {
         return llistaFreqPredeterminada;
     }
 
-    //Retorna la llista de paraules amb frequències de l
+    //Retorna la llista de paraules amb freqüències de l
     public  Map<String, Integer> getFrequencies() {
         return llistaFreqPredeterminada.getFrequencies();
     }
 
-    //Retorna informació (Nom de l'Idioma, nom de l'Alfabet i nom de la Llista de Freqüències predeterminada) de l'Idioma
+    //Retorna informació (nom, nom de l'alfabet i nom de la llista de freqüències predeterminada) de l'idioma
     public String getInfo() {
         return "Nom de l'Idioma: " + nom + "    Nom de l'Alfabet associat: " + alfabet.getNomAlfabet() +
                 "    Nom de la Llista de Freqüències predeterminada: " + llistaFreqPredeterminada.getNom();
