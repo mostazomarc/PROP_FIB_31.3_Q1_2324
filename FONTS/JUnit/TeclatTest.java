a15181d57b059e055429c0fbe2c19b4cd4d8c4dd
Agusti <agusti.costabella@estudiantat.upc.edu>
2023-11-18 14:39:11 +0100

    teclat.test en proces

--- FONTS/JUnit/TeclatTest.java
+++ FONTS/JUnit/TeclatTest.java
@@ -1,54 +1,77 @@
-//package JUnit;
-//
-//import Domini.*;
-//import Domini.LlistaFrequencies;
-//import Stubs.*;
-//
-//import static org.junit.Assert.*;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import java.util.*;
-//
-//public class TeclatTest {
-//
-//    private char[][] teclatQWERTY = {
-//            {'q', 'w', 'e','r', 't', 'y','u', 'i', 'o', 'p'},
-//            {'a', 's', 'd', 'f', 'g', 'h', 'k' , 'l', 'ñ'},
-//            {'z', 'x', 'c','v','b','n','m'}
-//    };
-//
-//    @Test
-//    public void crearTeclat() {
-//        LlistaFrequenciesStub llista = new LlistaFrequenciesStub();
-//        IdiomaStub idioma = new IdiomaStub();
-//        Teclat resultat = new Teclat("nom",llista,idioma);
-//        assertEquals("nom",resultat.getNomTeclat());
-//        assertEquals(teclatQWERTY,resultat.getTeclat());
-//    }
-//
-//    @Test
-//    public void obtenirDisposicióTeclat() {
-//        LlistaFrequenciesStub llista = new LlistaFrequenciesStub();
-//        IdiomaStub idioma = new IdiomaStub();
-//        Teclat resultat = new Teclat("nom",llista,idioma);
-//        assertEquals(teclatQWERTY,resultat.getTeclat());
-//    }
-//
-//    @Test
-//    public void obtenirNomTeclat() {
-//        LlistaFrequenciesStub llista = new LlistaFrequenciesStub();
-//        IdiomaStub idioma = new IdiomaStub();
-//        Teclat resultat = new Teclat("nom",llista,idioma);
-//        assertEquals("nom",resultat.getNomTeclat());
-//    }
-//
-//    @Test
-//    public void canviarTeclat() {
-//        LlistaFrequenciesStub llista = new LlistaFrequenciesStub();
-//        IdiomaStub idioma = new IdiomaStub();
-//        Teclat prova = new Teclat("nom",llista,idioma);
-//        assertNotNull(prova.canviaDisposicioTeclat());
-//    }
-//}
+package JUnit;
+
+import Domini.*;
+import Domini.LlistaFrequencies;
+import Stubs.*;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+
+public class TeclatTest {
+
+    private String nom;
+    private int dimX;
+    private int dimY;
+    private char[][] disposicio;
+    private Idioma idioma;
+    private LlistaFrequencies llistafreq;
+
+    private Set<Character> lletresProva = new HashSet<Character>();
+
+    Idioma idiomaProva;
+
+    private Map<String, Integer> llistaParaulesProva = new HashMap<>();
+
+
+    @Before
+    public void omplirLletres() {
+        for (char lletra = 'a'; lletra <= 'z'; lletra++) lletresProva.add(lletra);
+        Alfabet AlfabetProva = new Alfabet("Prova", lletresProva);
+        idiomaProva = new Idioma("ESPAÑOL",AlfabetProva);
+    }
+
+    @Before
+    public void omplirLlistaProva() {
+        llistaParaulesProva.put("Hola", 10);
+        llistaParaulesProva.put("Casa", 20);
+        llistaParaulesProva.put("Adeu", 30);
+        llistaParaulesProva.put("Menjar", 30);
+        llistaParaulesProva.put("Fideu", 30);
+    }
+    @Test
+    public void crearTeclatLlistaPròpia() {
+        LlistaFrequenciesStub llista = new LlistaFrequenciesStub();
+        IdiomaStub idioma = new IdiomaStub();
+        Teclat resultat = new Teclat("nom",llista,idioma);
+        assertEquals("nom",resultat.getNomTeclat());
+        assertEquals(teclatQWERTY,resultat.getTeclat());
+    }
+
+    @Test
+    public void crearTeclatLlistaIdioma() {
+        LlistaFrequenciesStub llista = new LlistaFrequenciesStub();
+        IdiomaStub idioma = new IdiomaStub();
+        Teclat resultat = new Teclat("nom",llista,idioma);
+        assertEquals(teclatQWERTY,resultat.getTeclat());
+    }
+
+    @Test
+    public void obtenirNomTeclat() {
+        LlistaFrequenciesStub llista = new LlistaFrequenciesStub();
+        IdiomaStub idioma = new IdiomaStub();
+        Teclat resultat = new Teclat("nom",llista,idioma);
+        assertEquals("nom",resultat.getNomTeclat());
+    }
+
+    @Test
+    public void canviarTeclat() {
+        LlistaFrequenciesStub llista = new LlistaFrequenciesStub();
+        IdiomaStub idioma = new IdiomaStub();
+        Teclat prova = new Teclat("nom",llista,idioma);
+        assertNotNull(prova.canviaDisposicioTeclat());
+    }
+}
