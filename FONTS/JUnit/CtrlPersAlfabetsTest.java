6b72a37a89e8042621afb9a5d7c19e7f286fec48
Arnau Tajahuerce <arnau.tajahuerce@estudiantat.upc.edu>
2023-11-18 23:14:29 +0100

    CtrlPersAlfabetsTest

---
+++ FONTS/JUnit/CtrlPersAlfabetsTest.java
@@ -0,0 +1,97 @@
+package JUnit;
+
+import ControladorsDomini.CtrlDomini;
+import Dades.CtrlPersAlfabets;
+import Domini.Alfabet;
+import Excepcions.AlfabetJaExisteix;
+import Excepcions.AlfabetNoExisteix;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class CtrlPersAlfabetsTest {
+    CtrlPersAlfabets cP;
+
+    CtrlDomini cD;
+
+    List<String> LlistaLlegidaProva;
+
+    Set<Character> lletresProva;
+
+    @Before
+    public void omplirLlistaLlegidaiLletres() {
+        LlistaLlegidaProva = new ArrayList<>();
+        LlistaLlegidaProva.add("abcd");
+        lletresProva = new HashSet<>();
+        lletresProva.add('a'); lletresProva.add('b'); lletresProva.add('c'); lletresProva.add('d');
+    }
+
+    @Before
+    public void getControladorDomini() {
+        cD = CtrlDomini.getInstance();
+    }
+
+    @Before
+    public void getControladorPersistencia() {
+        cP = CtrlPersAlfabets.getInstance(cD);
+    }
+
+    @Test
+    public void getControladorUnCopCreat() {
+        assertSame(cP, CtrlPersAlfabets.getInstance(cD));
+    }
+
+    @Test
+    public void afegirAlfabet() throws Exception {
+        cP.afegirAlfabet("AlfabetProva.txt", LlistaLlegidaProva);
+        Alfabet resultat = cP.getAlfabet("AlfabetProva");
+        assertEquals("AlfabetProva", resultat.getNomAlfabet());
+        assertEquals(lletresProva, resultat.getLletres());
+    }
+
+    @Test
+    public void eliminarAlfabet() throws Exception {
+        cP.afegirAlfabet("AlfabetProva.txt", LlistaLlegidaProva);
+        cP.eliminarAlfabet("AlfabetProva");
+        try {
+            cP.getAlfabet("AlfabetProva");
+        } catch (AlfabetNoExisteix e) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void afegirAlfabetDuplicat() throws Exception {
+        cP.afegirAlfabet("AlfabetProva.txt", LlistaLlegidaProva);
+        try {
+            cP.afegirAlfabet("AlfabetProva.txt", LlistaLlegidaProva);
+            fail();
+        } catch (AlfabetJaExisteix e) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void eliminarAlfabetInexistent() throws Exception {
+        try {
+            cP.eliminarAlfabet("AlfabetProva");
+            fail();
+        } catch (AlfabetNoExisteix e) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void getAlfabet() throws Exception {
+        cP.afegirAlfabet("AlfabetProva.txt", LlistaLlegidaProva);
+        Alfabet resultat = cP.getAlfabet("AlfabetProva");
+        assertEquals("AlfabetProva", resultat.getNomAlfabet());
+        assertEquals(lletresProva, resultat.getLletres());
+    }
+
+}
+
+//Classe Programada per: Arnau Tajahuerce
