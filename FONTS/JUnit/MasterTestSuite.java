b8badb476729e9868308fffc23cf389590666687
marc.mostazo <marc.mostazo@estudiantat.upc.edu>
2023-11-21 17:35:54 +0100

    canviat MasterTestSuite i error al CtrlPersTeclatTest

--- FONTS/JUnit/MasterTestSuite.java
+++ FONTS/JUnit/MasterTestSuite.java
@@ -8,7 +8,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(value = Suite.class)
 // Afegeix al array <nomtest>.class de la classe que vols que es comprovi al fer # make fulltest
 @SuiteClasses(value = {
-        AlfabetTest.class, IdiomaTest.class, LlistaFrequenciesTest.class, PerfilTest.class, CtrlPersPerfilTest.class, CtrlPersFreqTest.class, TeclatTest.class, CtrlPersTeclatsTest.class, HungarianAlgorithmTest.class
+        AlfabetTest.class, BranchandBoundTest.class, CtrlPersAlfabetsTest.class, CtrlPersFreqTest.class, CtrlPersIdiomesTest.class, CtrlPersIdiomesTest.class, CtrlPersPerfilTest.class,
+        CtrlPersTeclatsTest.class, GreedyTest.class, HungarianAlgorithmTest.class, IdiomaTest.class, LlistaFrequenciesTest.class, NodoTest.class, PerfilTest.class, posTest.class, TeclatTest.class
 })
 public class MasterTestSuite {
 }
