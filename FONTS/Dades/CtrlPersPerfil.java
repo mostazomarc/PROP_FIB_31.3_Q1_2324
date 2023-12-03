8bdbfc1c9fd9fff1509a62c94fbda738d103870e
Agusti <agusti.costabella@estudiantat.upc.edu>
2023-12-03 12:34:24 +0100

    perfils creat

--- FONTS/Dades/CtrlPersPerfil.java
+++ FONTS/Dades/CtrlPersPerfil.java
@@ -182,6 +182,7 @@ public class CtrlPersPerfil {
     private void afegirPerfil(String nomPerfil) throws PerfilJaExisteix {
         if (perfilExisteix(nomPerfil)) throw new PerfilJaExisteix(nomPerfil);
         perfilActual = new Perfil(nomPerfil);
+        guardar();
     }
 
     /**
@@ -240,6 +241,4 @@ public class CtrlPersPerfil {
         }
         return nomsPerfils;
     }
-
-
 }
