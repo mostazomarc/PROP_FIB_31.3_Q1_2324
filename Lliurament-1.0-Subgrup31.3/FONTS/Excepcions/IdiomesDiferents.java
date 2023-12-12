c57daa52434af0618bf55352e13653f646c5f2ba
marc.mostazo <marc.mostazo@estudiantat.upc.edu>
2023-12-12 20:12:42 +0100

    Esborrat lliurament 1 descomprimit

--- Lliurament-1.0-Subgrup31.3/FONTS/Excepcions/IdiomesDiferents.java
+++
@@ -1,9 +0,0 @@
-package Excepcions;
-
-public class IdiomesDiferents extends ExcepcionsCreadorTeclat {
-    public String getTipusExcepcio() {
-        return "IdiomesDiferents";
-    }
-
-    public IdiomesDiferents(String nomidiomallista, String nomidiomateclat) {super("Idiomes diferents: l'idioma de la llista:  " + nomidiomallista  + " es diferent a l'idioma del teclat que es vol crear: " + nomidiomateclat );}
-}
