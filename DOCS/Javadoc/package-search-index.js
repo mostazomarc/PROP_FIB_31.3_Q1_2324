b6e08a83baaacb59ead1a322cf20f4e50b05118e
marc.mostazo <marc.mostazo@estudiantat.upc.edu>
2023-12-22 14:47:33 +0100

    Carpeta DOCS sense JavaDoc

--- DOCS/Javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ControladorsDomini"},{"l":"Dades"},{"l":"Domini"},{"l":"Drivers"},{"l":"Excepcions"},{"l":"JUnit"},{"l":"Presentacio"},{"l":"Presentacio.views"},{"l":"Stubs"}]
