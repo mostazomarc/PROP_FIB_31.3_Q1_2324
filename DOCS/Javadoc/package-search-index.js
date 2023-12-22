f9fd875f7558f8af409f9bf1930e6add1f0f9ae2
marc.mostazo <marc.mostazo@estudiantat.upc.edu>
2023-12-22 17:25:53 +0100

    executable final i JavaDoc

---
+++ DOCS/Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ControladorsDomini"},{"l":"Dades"},{"l":"Domini"},{"l":"Drivers"},{"l":"Excepcions"},{"l":"JUnit"},{"l":"Presentacio"},{"l":"Presentacio.views"},{"l":"Stubs"}]
